479bf9eea386dea883abcd8aad52e63fdce7c299
Nelson Martell <nelson6e65@gmail.com>
2023-10-11 11:34:37 -0500

    feat(presets): define default preset

--- src/presets/default.ts
+++ src/presets/default.ts
@@ -1,5 +1,40 @@
-const config = {
-  dev: true,
+import { Linter } from 'eslint';
+
+import { baseRules, getImportRules, getNodeRules } from '~/rules';
+
+const config: Linter.Config = {
+  overrides: [
+    {
+      files: ['*.js', '*.mjs', '*.cjs'],
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+      extends: [
+        //
+        'eslint:recommended',
+        'plugin:node/recommended',
+        'plugin:import/recommended',
+        'plugin:unicorn/recommended',
+        'prettier',
+      ],
+      plugins: [
+        //
+        'node',
+        'import',
+        'unicorn',
+      ],
+      rules: {
+        ...baseRules,
+        ...getImportRules({
+          pathsGroups: [
+            { group: 'internal', pattern: '@/**' },
+            { group: 'internal', pattern: '~/**' },
+          ],
+        }),
+        ...getNodeRules({ nodeVersionRange: '>=16' }),
+      },
+    },
+  ],
 };
 
 export default config;
