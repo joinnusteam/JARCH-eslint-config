934a45d383f77dc5f2f571d2eaaa344171dcda5d
Nelson Martell <nelson6e65@gmail.com>
2023-10-11 17:07:28 -0500

    fix(rules): adjust some rules as warning
    - unicorn/filename-case
    - unicorn/prevent-abbreviations

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -7,4 +7,27 @@ export const baseRules: Linter.RulesRecord = {
   'unicorn/no-null': 'off',
   'no-console': 'warn',
   'unicorn/prefer-module': 'warn',
+  'unicorn/filename-case': [
+    'warn',
+    {
+      case: 'kebabCase',
+    },
+  ],
+  'unicorn/prevent-abbreviations': [
+    'warn',
+    {
+      replacements: {
+        i: {
+          index: false,
+        },
+        e: {
+          event: false,
+        },
+        res: false,
+        cmd: {
+          command: true,
+        },
+      },
+    },
+  ],
 };
